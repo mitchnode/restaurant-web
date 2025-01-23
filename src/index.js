7edf786f41e327165694d5c16e9f6213bde80742
mitchnode <70460735+mitchnode@users.noreply.github.com>
2025-01-24 00:11:54 +1100

    Banner title and paragraph added via JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./styles.css";
+import { heading, banner, paragraph } from "./page-load";
 
 console.log("Welcome to Resturant 42");
+
+var content = document.getElementById("content")
+
+var img = document.createElement("img");
+img.src = banner;
+img.width = window.innerWidth;
+content.appendChild(img);
+
+var h1 = document.createElement("h1");
+h1.textContent = heading;
+content.appendChild(h1)
+
+var p = document.createElement("p");
+p.textContent = paragraph
+content.appendChild(p)
